54f13213515919d5bfa82cdc67748eff5a354540
Wanjiru Mburu <wanjirumarionne@gmail.com>
2024-02-04 14:13:42 +0300

    Completed React Forms Abstraction

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function Form() {
+/*function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
 
@@ -19,6 +19,60 @@ function Form() {
       <button type="submit">Submit</button>
     </form>
   );
+}*/
+
+function Form() {
+  const [formData, setFormData] = useState({
+    firstName: "John",
+    lastName: "Henry",
+  });
+
+  function handleFirstNameChange(event) {
+    setFormData({
+      ...formData,
+      firstName: event.target.value,
+    });
+  }
+
+  function handleLastNameChange(event) {
+    setFormData({
+      ...formData,
+      lastName: event.target.value,
+    });
+  }
+
+  /*return (
+    <form>
+      <input
+        type="text"
+        onChange={handleFirstNameChange}
+        value={formData.firstName}
+      />
+      <input
+        type="text"
+        onChange={handleLastNameChange}
+        value={formData.lastName}
+      />
+    </form>
+  );*/
+
+  return (
+    <form>
+      <input
+        type="text"
+        name="firstName"
+        value={formData.firstName}
+        onChange={handleFirstNameChange}
+      />
+      <input
+        type="text"
+        name="lastName"
+        value={formData.lastName}
+        onChange={handleLastNameChange}
+      />
+    </form>
+  );
+
 }
 
 export default Form;
